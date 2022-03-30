167f3afab4e18b3c77a9f54689cefc7ee83c56a5
Tiffany France <tiffany6872@gmail.com>
2022-03-30 15:15:58 -0400

    week 1 work

---
+++ week-01/week01-homework.js
@@ -0,0 +1,25 @@
+d3.json('covid.json').then(data => {
+  let height = 400,
+    width = 600,
+    margin = ({ top: 25, right: 30, bottom: 35, left: 40 });
+
+  //DEFINE X SCALE HERE
+
+  //DEFINE Y SCALE HERE
+
+  //DEFINE X AXIS HERE
+
+  //DEFINE Y AXIS HERE
+
+  //CREATE SVG HERE
+
+  //APPEND X AXIS
+
+  //APPEND Y AXIS
+
+  //CREATE BAR GROUP
+
+  //CREATE BARS AND ADD TO GROUP
+
+  //ADD TEXT TO BARS 
+});
